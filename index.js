1f9e88acd89b8849a545268b2accec7d05638efc
Levy <levypoyaoan45@gmail.com>
2026-08-06 08:56:37 +0800

    telegram

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const axios = require("axios");
 const BOT_TOKEN = "8968729641:AAFZjjFZo55YiTLE1tk6cLNZ0YsyE5Gf-l0";
 const CHAT_ID = "5279773215";
 
+
+// If using Render environment variable:
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// If testing locally with service-account.json instead, use:
+// const serviceAccount = require("./service-account.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+app.use(express.json());
+
 async function sendTelegram(message) {
     try {
         const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
@@ -30,18 +44,6 @@ app.get("/telegram", async (req, res) => {
     res.send("Telegram message sent!");
 });
 
-// If using Render environment variable:
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-// If testing locally with service-account.json instead, use:
-// const serviceAccount = require("./service-account.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-const app = express();
-app.use(express.json());
 
 // Home page
 app.get("/", (req, res) => {
